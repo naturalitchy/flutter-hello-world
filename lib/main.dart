fdcdecbc9fa292476cf3004d1164efab858082f1
naturalitchy <nsubway635@gmail.com>
2023-01-28 18:59:27 +0900

    Add children code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: Scaffold(
+//         body: Center (
+//           child: Text('Hello World!!@@@'),
+//         ),
+//       ),
+//     ),
+//   );
+// }
+
+
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center (
-          child: Text('Hello World!!@@@'),
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Coce'),
+              Text('Factory'),
+            ],
+          )
         ),
-      ),
+      )
     ),
   );
 }
